c49f4988ccae1c6f79fe091e8c2b1aaa35cdb2fe
Annanda Sousa <annanda.sousa@gmail.com>
2016-06-04 00:42:25 -0300

    Construindo o y_ (classificacao esperada), usando indices

---
+++ prepare_data.py
@@ -0,0 +1,43 @@
+import csv
+
+
+class Clean:
+
+    x = []
+    y = []
+
+    def __init__(self, file_path):
+        self.read_csv(file_path)
+
+    def read_csv(self, file_path):
+        with open(file_path, 'r') as csv_file:
+            exemples = csv.reader(csv_file, delimiter=',')
+
+            for i, row in enumerate(exemples):
+                if i != 0 and i < 10:
+                    xi = []
+                    xi.append(row[2])
+                    xi.append(row[3])
+                    xi.append(row[4])
+                    xi.append(row[5])
+                    xi.append(row[6])
+                    xi.append(row[7])
+                    self.y.append(self.get_classification(row[1]))
+                    self.x.append(xi)
+
+            print("Esse é o", self.x)
+            print("Esse é o y", self.y)
+
+    def get_classification(self, name):
+
+        classifier = {
+            'Return_to_owner': 0,
+            'Euthanasia': 1,
+            'Adoption': 2,
+            'Transfer': 3,
+            'Died': 4
+        }
+        return classifier[name]
+
+
+cleaned = Clean('train.csv')
